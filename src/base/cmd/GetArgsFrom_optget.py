b4100671e300cf27dce76ef3edf491794827920d
0opslab <909070781@qq.com>
2018-01-24 21:03:01 +0800

    python

--- src/base/cmd/GetArgsFrom_optget.py
+++ src/base/cmd/GetArgsFrom_optget.py
@@ -8,11 +8,11 @@ import sys, getopt
 def main(argv):
     try:
         opts, args = getopt.getopt(argv, "hg:d")
-        print "opt's type=>", type(opts), opts
-        print "args's type=>", type(args), args
+        print("opt's type=>", type(opts), opts)
+        print("args's type=>", type(args), args)
 
         for opt, arg in opts:
-            print opt, arg
+            print (opt, arg)
 
     except getopt.GetoptError:
         sys.exit(2)
--- src/tools/find_repet_file.py
+++ src/tools/find_repet_file.py
@@ -1,30 +1,19 @@
 #! /usr/bin/python
 # coding=UTF-8
 # version:python3.x
-import os
 import hashlib
+import optparse
+import os
 
 """根据文件hash查找指定目录下的重复文件"""
-# def get_hash(file_path):
-#     f = open(file_path, 'rb')
-#     md5_obj = hashlib.md5()
-#     while True:
-#         d = f.read(1024 * 10)
-#         if not d:
-#             break
-#         md5_obj.update(d)
-#     hash_code = md5_obj.hexdigest()
-#     f.close()
-#     md5 = str(hash_code).lower()
-#     return md5
 
 
-def hash_file(fineName, type="sha256", block_size=64 * 1024):
+def hash_file(fine_name, type="sha256", block_size=64 * 1024):
     """ Support md5(), sha1(), sha224(), sha256(), sha384(), sha512(),
     blake2b(), blake2s(),sha3_224, sha3_256, sha3_384, sha3_512,
     shake_128, and shake_256
     """
-    with open(fineName, 'rb') as file:
+    with open(fine_name, 'rb') as file:
         hash = hashlib.new(type, b"")
         while True:
             data = file.read(block_size)
@@ -35,21 +24,29 @@ def hash_file(fineName, type="sha256", block_size=64 * 1024):
 
 
 if __name__ == '__main__':
-    path = "D:/电子书/CODE"
-    hash_map = []
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            file_name = os.path.join(root, file)
-            # file_hash = get_hash(file_name)
-            file_hash = hash_file(file_name, "md5")
-            info = {'file': file_name, 'hash': file_hash}
-            hash_map.append(info)
+    usage = "*.py [ -d <path>]"
+    parser = optparse.OptionParser()
+    parser.add_option('-d', '--dir', dest="path", help="handle path", metavar="FILE")
+    (options, args) = parser.parse_args()
+    if options.path:
+        hash_map = []
+        for root, dirs, files in os.walk(options.path):
+            for file in files:
+                file_name = os.path.join(root, file)
+                # file_hash = get_hash(file_name)
+                file_hash = hash_file(file_name, "md5")
+                info = {'file': file_name, 'hash': file_hash}
+                hash_map.append(info)
 
-    result = {}
-    for item in hash_map:
-        for it in hash_map:
-            if it['hash'] == item['hash'] and it['file'] != item['file']:
-                result[it['hash']] = "{" + it['file'] + "," + item['file'] + "}"
+        result = {}
+        for item in hash_map:
+            for it in hash_map:
+                if it['hash'] == item['hash'] and it['file'] != item['file']:
+                    result[it['hash']] = "{" + it['file'] + "," + item['file'] + "}"
 
-    for (k, v) in result.items():
-        print("hash:", k, "\tfile:", v)
+        for (k, v) in result.items():
+            print(k, "\t", v.replace("\\","/"))
+    else:
+        print(parser.print_help())
+
+        # path = "D:/电子书/CODE"
